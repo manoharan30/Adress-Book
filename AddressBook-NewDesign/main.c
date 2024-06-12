b7d8b923c8a0db5a3bc4995912666d3555ad72e8
manoharan30 <111949505+manoharan30@users.noreply.github.com>
2024-06-12 10:21:44 +0530

    Update main.c

--- AddressBook-NewDesign/main.c
+++ AddressBook-NewDesign/main.c
@@ -6,31 +6,31 @@ int main()
     initialize(&ads);
     int flag=1;
     while(flag){
-    int option;
+    char option;
     printf("\n1.Create\n2.Search\n3.Edit\n4.Delete\n5.List\n6.Exit\n");
     printf("\nEnter the option:");
-    scanf("%d",&option);
+    scanf("%c",&option);
     printf("\n");
         switch (option)
         {
-        case 1:
+        case '1':
             createContact(&ads);
             break;
-        case 2:
+        case '2':
             searchContact(&ads);
             break;
-        case 3:
+        case '3':
             searchContact(&ads);
             editContact(&ads);
             break;
-        case 4:
+        case '4':
             searchContact(&ads);
             deleteContact(&ads);
             break;
-        case 5:
+        case '5':
             listContacts(&ads);
             break;
-        case 6:
+        case '6':
             flag=0;
             break;
         default:
